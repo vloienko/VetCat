18220daa64925ed740738ee4e06eaf0763d796bc
Maksym Martynenko <predatar24@gmail.com>
2023-06-29 13:57:01 +0300

    Made a second screen (Swiper) of the Services page

--- src/js/app.js
+++ src/js/app.js
@@ -4,15 +4,17 @@
 // import Swiper, { Navigation, Pagination } from 'swiper';
 // const swiper = new Swiper();
 
-
 //<MODULES static>=============================
 // import './modules/_static/';
 // import './modules/_static/';
 //</MODULES static>============================
 
 // //<MODULES dynamic>============================
-import burger from "./modules/main/header-sidebar.js";
-import sidebar from "./modules/main/header-sidebar.js";
+import burger from './modules/main/header-sidebar.js';
+import sidebar from './modules/main/header-sidebar.js';
+
+import swiper from './modules/libs/swiper.js';
+import addEventListenerForServicesToggle from './modules/pages/services.js';
 
 // import './modules/main/';
 // import './modules/main/';
