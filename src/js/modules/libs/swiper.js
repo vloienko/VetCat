7c2bc6ec7079532224c3f51c1e54d03f7bcca71b
Maksym Martynenko <predatar24@gmail.com>
2023-06-30 22:48:02 +0300

    Сделал "Фотогалерею" и просмотр картинок без адаптации

--- src/js/modules/libs/swiper.js
+++ src/js/modules/libs/swiper.js
@@ -1,4 +1,4 @@
-import Swiper, { Navigation } from 'swiper';
+import Swiper from 'swiper';
 
 function addSwiperForSpecialistSwiper() {
   if (document.querySelector('.specialist__swiper')) {
@@ -14,6 +14,24 @@ function addSwiperForSpecialistSwiper() {
     });
     document.querySelector('.specialist__swiper-prev').addEventListener('click', () => {
       specialistSwiper.slidePrev();
+    });
+  }
+}
+
+function addSwiperForOurMissionSwiper() {
+  if (document.querySelector('.our-mission__swiper')) {
+    const ourMissionSwiper = new Swiper('.our-mission__swiper', {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+      loop: true,
+      grabCursor: true
+    });
+
+    document.querySelector('.our-mission__swiper-next').addEventListener('click', () => {
+      ourMissionSwiper.slideNext();
+    });
+    document.querySelector('.our-mission__swiper-prev').addEventListener('click', () => {
+      ourMissionSwiper.slidePrev();
     });
   }
 }
