d4ad0bf6a1050bc052475d422c11ea8fe4c479e3
Владіслав Лоєнко <74206645+vloienko@users.noreply.github.com>
2023-07-05 00:55:20 +0300

    cost-services page - done

--- src/js/app.js
+++ src/js/app.js
@@ -9,22 +9,28 @@
 // import './modules/_static/';
 //</MODULES static>============================
 
+// Libs
+import './modules/libs/swiper.js';
+import './modules/libs/mixitup.js';
+
 // //<MODULES dynamic>============================
 import burger from './modules/main/header-sidebar.js';
 import sidebar from './modules/main/header-sidebar.js';
 // index.html
 import petServiceSwiper from './modules/libs/swiper.js';
+// cost-services.html
+import mixitupFilter from './modules/libs/mixitup.js';
+
+
 
 
 import './modules/libs/common.js';
-
-import './modules/libs/swiper.js';
 import './modules/pages/services.js';
 import './modules/pages/contacts.js';
 import './modules/pages/info.js';
 import './modules/pages/about-us.js';
 import './modules/pages/search.js';
-import './modules/pages/timetable.js';
+// import './modules/pages/timetable.js';
 
 // import './modules/main/';
 // import './modules/main/';
