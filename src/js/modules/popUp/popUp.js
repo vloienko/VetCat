a450cb2b5586b1052cc7120aada1a9a39d2c94a0
Maksym Martynenko <predatar24@gmail.com>
2023-07-04 14:15:38 +0300

    add popUp

--- src/js/modules/popUp/popUp.js
+++ src/js/modules/popUp/popUp.js
@@ -1,28 +1,44 @@
 function initSelectInFeedback() {
-  const select = document.querySelector('.popUp-feedback__select');
-  const selected = document.querySelector('.popUp-feedback__selected');
-  const selectList = document.querySelector('.popUp-feedback__list');
-  const selectListItem = document.querySelectorAll('.popUp-feedback__list-item');
+  if (document.querySelector('.popUp-feedback__select')) {
+    const select = document.querySelector('.popUp-feedback__select');
+    const selected = document.querySelector('.popUp-feedback__selected');
+    const selectList = document.querySelector('.popUp-feedback__list');
+    const selectListItem = document.querySelectorAll('.popUp-feedback__list-item');
 
-  const toggleItem = index => {
-    selectListItem.forEach(elem => {
-      elem.classList.contains('popUp-feedback__list-item_active')
-        ? elem.classList.remove('popUp-feedback__list-item_active')
-        : null;
-      selectListItem[index].classList.add('popUp-feedback__list-item_active');
-      selected.childNodes[0].textContent = selectListItem[index].innerHTML;
+    const toggleItem = index => {
+      selectListItem.forEach(elem => {
+        elem.classList.contains('popUp-feedback__list-item_active')
+          ? elem.classList.remove('popUp-feedback__list-item_active')
+          : null;
+        selectListItem[index].classList.add('popUp-feedback__list-item_active');
+        selected.childNodes[0].textContent = selectListItem[index].innerHTML;
+      });
+    };
+
+    select.addEventListener('click', () => {
+      selectList.classList.toggle('popUp-feedback__list_show');
     });
-  };
 
-  select.addEventListener('click', () => {
-    selectList.classList.toggle('popUp-feedback__list_show');
-  });
-
-  selectListItem.forEach((elem, index) => {
-    elem.addEventListener('click', e => {
-      toggleItem(index);
+    selectListItem.forEach((elem, index) => {
+      elem.addEventListener('click', e => {
+        toggleItem(index);
+      });
     });
-  });
+  }
+}
+function initToggleInReception() {
+  if (document.querySelector('.popUp-reception__toggle')) {
+    const toggleItems = document.querySelectorAll('.popUp-reception__toggle div');
+
+    toggleItems.forEach((elem, index) => {
+      elem.addEventListener('click', e => {
+        toggleItems.forEach(elem => {
+          elem.classList.contains('toggle__item_active') ? elem.classList.remove('toggle__item_active') : null;
+        });
+        toggleItems[index].classList.add('toggle__item_active');
+      });
+    });
+  }
 }
 
 function showFeedback() {
@@ -45,5 +61,27 @@ function showFeedback() {
     });
   }
 }
+function showReception() {
+  if (document.querySelector('.btn_reception')) {
+    const btnsReception = document.querySelectorAll('.btn_reception');
+    const closeReception = document.querySelectorAll('.popUp-reception__close');
+    const popUp = document.querySelector('.popUp-reception');
+
+    btnsReception.forEach(elem => {
+      elem.addEventListener('click', () => {
+        popUp.classList.add('popUp-reception_show');
+      });
+    });
+
+    closeReception.forEach(elem => {
+      elem.addEventListener('click', e => {
+        popUp.classList.remove('popUp-reception_show');
+      });
+    });
+  }
+}
 
 initSelectInFeedback();
+initToggleInReception();
+
+showReception();
